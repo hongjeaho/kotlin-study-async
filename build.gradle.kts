c74a334be9a6a1a939acd0a4456e4e815a3bcd5c
hongjeaho <kansin88@gmail.com>
2023-10-17 23:41:09 +0900

    코틀린 비동기 호출 공부용 프로젝트 생셩

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("io.github.microutils:kotlin-logging:1.4.3")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("io.projectreactor:reactor-core:3.5.11")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
